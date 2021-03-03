3c01cc58176749f8f7ba416269700150bbe189c5
pom421 <mauguet.po@gmail.com>
2021-03-03 11:15:38 +0100

    fix: error in .eslintrc

--- src/__tests__/unit/components/CurrentMonthEmployments.spec.js
+++ src/__tests__/unit/components/CurrentMonthEmployments.spec.js
@@ -1,42 +1,41 @@
-import * as nextRouter from "next/router"
-import React from "react"
 import { render, screen, waitFor } from "@testing-library/react"
+import React from "react"
 
-import { CurrentMonthEmployments } from "../../../components/EmploymentMonthData"
-import { SUPER_ADMIN } from "../../../utils/roles"
-import * as auth from "../../../utils/auth"
 import * as clientsEmployment from "../../../clients/employments"
 import * as clientsReference from "../../../clients/employments-references"
+import { CurrentMonthEmployments } from "../../../components/EmploymentMonthData"
+import * as auth from "../../../utils/auth"
+import { SUPER_ADMIN } from "../../../utils/roles"
 
 jest.spyOn(auth, "getCurrentUser")
 jest.spyOn(clientsEmployment, "findEmployment")
 jest.spyOn(clientsReference, "searchReferenceForMonth")
 
 const lambdaUser = {
+    hospital: { id: 3 },
     id: 3,
     role: SUPER_ADMIN,
-    hospital: { id: 3 },
 }
 
 const lambdaEmployments = {
-    ides: "3.3",
     auditoriumAgents: "1",
-    others: "2",
     doctors: "11.9",
-    secretaries: "1.9",
-    nursings: "4.15",
     executives: "1",
+    ides: "3.3",
+    nursings: "4.15",
+    others: "2",
+    secretaries: "1.9",
 }
 
 const lambdaReferences = {
     reference: {
-        ides: "0.5",
         auditoriumAgents: "1",
-        others: "1.5",
         doctors: "0",
-        secretaries: "0",
-        nursings: "0",
         executives: "0",
+        ides: "0.5",
+        nursings: "0",
+        others: "1.5",
+        secretaries: "0",
     },
 }
 
@@ -57,7 +56,9 @@ afterEach(() => {
 })
 
 it("should render CurrentMonthEmployments", async () => {
-    await waitFor(() => render(<CurrentMonthEmployments month={lambdaMonth} year={lambdaYear} hospitalId={lambdaHospitalId} />))
+    await waitFor(() =>
+        render(<CurrentMonthEmployments month={lambdaMonth} year={lambdaYear} hospitalId={lambdaHospitalId} />),
+    )
 
     expect(screen.getByLabelText(/secrétaire/i)).toHaveValue(1.9)
     expect(screen.getByLabelText(/reference.others/i).textContent).toMatchInlineSnapshot(`"1.5 ETP prévus"`)
@@ -65,5 +66,4 @@ it("should render CurrentMonthEmployments", async () => {
     expect(auth.getCurrentUser).toHaveBeenCalled()
     expect(clientsEmployment.findEmployment).toHaveBeenCalled()
     expect(clientsReference.searchReferenceForMonth).toHaveBeenCalled()
-
 })
